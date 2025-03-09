f269bdd058850dfad7e776d1df8d81bb988fe212
Ravinagraj01 <ravi.nagraj01@gmail.com>
2025-03-09 18:59:47 +0530

    structure created

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+      </h1>
+      <Button>LMS </Button>
+    </>
+  )
+}
+
+export default App
